46fa1cea70add470d18d886ab2f2a2429bdef195
Alexander Govorukhin <alexandergdev20@gmail.com>
2025-10-17 13:07:40 +0500

    Первое задание выполнено

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,17 @@
+package ru.oop.task1;
+
+/**
+ * Машина
+ */
+public class Car {
+    /**
+     * Передвижение до нужного места
+     * Может не доехать до места назначения
+     * @param person
+     * @param destination
+     * @return
+     */
+    public void move(Person person, Position destination) {
+        //TODO
+    }
+}
--- src/main/java/ru/oop/task1/MainTask1.java
+++ src/main/java/ru/oop/task1/MainTask1.java
@@ -22,7 +22,8 @@ public class MainTask1 {
      * Переехать из текущего места в заданную точку
      */
     public void moveTo(Person person, Position destination) {
-        // TODO
+        new Car().move(person, destination);
+        person.walk(destination);
         assert person.getPosition() == destination;
     }
 }
